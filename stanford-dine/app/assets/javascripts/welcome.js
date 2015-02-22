3f42485a5c0e79156a7abe7358bc25b1de1f95b9
Jason Liu <liujas000@gmail.com>
2015-02-21 19:50:25 -0800

    commit

--- stanford-dine/app/assets/javascripts/welcome.js
+++ stanford-dine/app/assets/javascripts/welcome.js
@@ -1,3 +1,15 @@
+<<<<<<< HEAD
+$('#plus-button').on('click', function() {
+  $(this).button('toggle');
+  alert("BUTTON WAS PRESSED");
+  console.log("button pressed");
+});
+
+$(function(){
+    $('#newThread').on('click', function(e){
+        alert("BUTTON WAS PRESSED");
+        console.log("button pressed");
+=======
 $(function(){
     $('#plus-button').on('click', function(e){
         alert("BUTTON WAS PRESSED");
@@ -6,5 +18,6 @@ $(function(){
           type:'POST',
           url:'/vote/'+ $(this).attr('id') + '/1';
         })
+>>>>>>> 83168ecabe7b7896378f76b3116602557f82b34f
     });
 });
