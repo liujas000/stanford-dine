aa594d07ba9a12a7fec12c3e8156eb9d195a9369
Jason Liu <liujas000@gmail.com>
2015-02-21 18:23:34 -0800

    shit

--- stanford-dine/db/migrate/20150222021441_create_halls.rb
+++ stanford-dine/db/migrate/20150222021441_create_halls.rb
@@ -1,9 +1,8 @@
 class CreateHalls < ActiveRecord::Migration
   def change
     create_table :halls do |t|
-      t.string :id
       t.string :name
-      t.string :img, 
+      t.string :img
       t.integer :score
 
       t.timestamps null: false
