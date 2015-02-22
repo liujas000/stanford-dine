83168ecabe7b7896378f76b3116602557f82b34f
Max Spero <mmspero@gmail.com>
2015-02-21 18:29:33 -0800

    button stuff

--- stanford-dine/app/controllers/welcome_controller.rb
+++ stanford-dine/app/controllers/welcome_controller.rb
@@ -2,4 +2,9 @@ class WelcomeController < ApplicationController
   def index
     @halls = Hall.all
   end
+
+  def vote
+    @hall = Article.find(params[:id])
+    vote_val = params[:val]; 
+  end
 end
